9487f9e94aa32d713ad3e50e7b611740cda73148
porawiec <54656651+porawiec@users.noreply.github.com>
2020-05-08 20:48:09 -0500

    created postRegister method controller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
 const User = require('./models/user');
+const session = require('express-session');
 
 // require routes
 const indexRouter = require('./routes/index');
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { postRegister } = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -12,14 +13,10 @@ router.get('/register', (req, res, next) => {
 });
 
 /* POST /register */
-router.post('/register', (req, res, next) => {
-  res.send('POST /register');
-});
+router.post('/register', postRegister);
 
 /* GET /login */
-router.get('/login', (req, res, next) => {
-  res.send('GET /login');
-});
+router.get('/login', postRegister);
 
 /* POST /login */
 router.post('/login', (req, res, next) => {
