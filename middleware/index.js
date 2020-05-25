3ed4f969b368486926895f355c662a886fe8c041
porawiec <54656651+porawiec@users.noreply.github.com>
2020-05-24 22:58:51 -0500

    created more restful and clearer naming conventions

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     // handle promise/async errors
-    errorHandler: (fn) =>
+    asyncErrorHandler: (fn) =>
         (req, res, next) => {
             Promise.resolve(fn(req, res, next))
                 .catch(next);
