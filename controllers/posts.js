2b138ae5ea0c3e663e1c2798d6d82b695b9347c5
porawiec <54656651+porawiec@users.noreply.github.com>
2020-05-29 23:49:48 -0500

    added cloudinary images support. added input type for images to new.ejs. created additional object properties and support for images in post controller. image support added in POST of post route. needed to change datatypes needed for images in post model.

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,10 @@
 const Post = require('../models/post');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name: 'porawiec',
+    api_key: '671774781274648',
+    api_secret: process.env.CLOUDINARY_SECRET
+})
 
 module.exports = {
     // Posts Index
@@ -14,6 +20,14 @@ module.exports = {
 
     // Posts Create
     async postCreate(req, res, next) {
+        req.body.post.images = [];
+        for(const file of req.files) {
+            let image = await cloudinary.v2.uploader.upload(file.path);
+            req.body.post.images.push({
+                url: image.secure_url,
+                public_id: image.public_id
+            });
+        }
         // use req.body to create a new Post
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`)
