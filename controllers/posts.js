0b1c208e1a2f75298e28fbcf462c37476b000163
porawiec <54656651+porawiec@users.noreply.github.com>
2020-06-02 20:26:35 -0500

    created backend logic for post update method to find and delete images from cloudinary and post.images by its public_id while maintaining a max images count of a post at 4

--- controllers/posts.js
+++ controllers/posts.js
@@ -47,7 +47,44 @@ module.exports = {
 
     // Posts Update
     async postUpdate(req, res, next) {
-        let post = await Post.findByIdAndUpdate(req.params.id, req.body.post);
+        // find post by id
+        let post = await Post.findById(req.params.id);
+        // check if there are any images queued for deletion
+        if(req.body.deleteImages && req.body.deleteImages.length) {
+            let deleteImages = req.body.deleteImages;
+            // loop over deleteImages and delete images from cloudinary and post.images
+            for(const public_id of deleteImages) {
+                // delete images from cloudinary
+                await cloudinary.v2.uploader.destroy(public_id);
+                // delete images from cloudinary
+                for (const image of post.images) {
+                    if(image.public_id === public_id) {
+                        let index = post.images.indexOf(image);
+                        post.images.splice(index,1);
+                    }
+                }
+            }
+        }
+        // check if new images are queued for upload
+        if(req.files){
+            // upload images
+            for(const file of req.files) {
+                let image = await cloudinary.v2.uploader.upload(file.path);
+                // add images to post.images array
+                post.images.push({
+                    url: image.secure_url,
+                    public_id: image.public_id
+                });
+            }
+        }
+        // update post with properties submitted from the edit form
+        // note: this will also update equal values
+        post.title = req.body.post.title;
+        post.description = req.body.post.description;
+        post.price = req.body.post.price;
+        post.location = req.body.post.location;
+
+        post.save();
         res.redirect(`/posts/${post.id}`) // could also work with req.params.id
      },
     // Posts Destroy
