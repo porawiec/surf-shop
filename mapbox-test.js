3dcd51f9531ab1951351aed18b39493a63769cbc
porawiec <54656651+porawiec@users.noreply.github.com>
2020-06-05 00:16:22 -0500

    set up geocoding client. created async requests to api to deliver geocoordinates for location in test file.

--- mapbox-test.js
+++ mapbox-test.js
@@ -1,3 +1,20 @@
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const geocodingClient = mbxGeocoding({ accessToken: 'your-access-token' });
+require('dotenv').config();
 
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
+
+async function geocoder(location) {
+    try {
+        let response = await geocodingClient
+            .forwardGeocode({
+                query: location,
+                limit: 1
+            })
+            .send()
+
+    console.log(response.body.features[0].geometry.coordinates);
+} catch(err) {
+    console.log(err.message);
+    }
+}
+geocoder('Alaska, US')
