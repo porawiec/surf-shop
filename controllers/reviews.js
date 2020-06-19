0c27600628b6df5c3a00ba5a5f5cc0ca184e644d
porawiec <54656651+porawiec@users.noreply.github.com>
2020-06-18 20:23:21 -0500

    created create review on post and show review on post.

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,8 +4,20 @@ const Review = require('../models/review');
 
 module.exports = {
 
-    // Reviewss Create
+    // Reviews Create
     async reviewCreate(req, res, next) {
+        // find post by id
+        let post = await Post.findById(req.params.id);
+        // create the review
+        // req.body.review.author = req.user_id;
+        let review = await Review.create(req.body.review);
+        // assign review to post
+        post.reviews.push(review);
+        // save the post
+        post.save();
+        req.session.success = 'Review created successfully!';
+        //redirect to post
+        res.redirect(`/posts/${post.id}`)
 
     },
 
@@ -13,7 +25,7 @@ module.exports = {
     async reviewUpdate(req, res, next) {
 
     },
-    
+
     // Reviews Destroy
     async reviewDestroy(req, res, next) {
 
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
+const { asyncErrorHandler } = require('../middleware');
+const {
+    reviewCreate,
+    reviewUpdate,
+    reviewDestroy
+} = require('../controllers/reviews')
 
 /* POST reviews create /posts/:id/reviews */
-router.post('/', (req, res, next) => {
-    res.send('CREATE /reviews')
-});
+router.post('/', asyncErrorHandler(reviewCreate));
 
 /* PUT reviews update /posts/:id/reviews/:review_id */
 router.put('/:review_id', (req, res, next) => {
