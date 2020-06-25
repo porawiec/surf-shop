f22adc868ab7c28a6e31a24f1ba347f521f9985d
porawiec <54656651+porawiec@users.noreply.github.com>
2020-06-24 23:18:52 -0500

    created authentication middleware for author of a review for backend. frontend authentication to display only current user's reviews to edit.

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,22 @@
+const Review = require('../models/review');
+
 module.exports = {
     // handle promise/async errors
     asyncErrorHandler: (fn) =>
         (req, res, next) => {
             Promise.resolve(fn(req, res, next))
                 .catch(next);
+        },
+    isReviewAuthor: async (req, res, next) => {
+        // find the review
+        let review = await Review.findById(req.params.review_id);
+        // check to see if the author of the review is current user logged in whos trying to edit
+        if(review.author.equals(req.user._id)) {
+            // run next method in middleware chain if ===
+            return next()
         }
+        // flash message and redirect to root route
+        req.session.error = 'You do not have access to edit this Review.';
+        return res.redirect('/');
+    }
 }
