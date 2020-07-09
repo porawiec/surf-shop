c08c979c56e9cb8ddf7a854758fecdd6ed6d526a
porawiec <54656651+porawiec@users.noreply.github.com>
2020-07-08 19:33:09 -0500

    added mongoose paginate to model post. added paginate method to postIndex to replace .find. added .docs when iterating in index.ejs view to accommodate new mongoose pagination.

--- models/post.js
+++ models/post.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
+const mongoosePaginate = require('mongoose-paginate');
 
 const PostSchema = new Schema({
     title: String,
@@ -30,5 +31,7 @@ PostSchema.pre('remove', async function() {
         }
     })
 })
+
+PostSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model('Post', PostSchema)
