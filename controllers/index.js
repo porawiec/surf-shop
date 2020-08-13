81dc2832984f78664cbf6d728fa32871ff43074d
porawiec <54656651+porawiec@users.noreply.github.com>
2020-08-12 22:32:58 -0500

    created an update profile method in controllers. added middleware to put route of updating profile.

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const Post = require('../models/post');
 const passport = require('passport');
 const mapBoxToken = process.env.MAPBOX_TOKEN;
+const util = require('util');
 
 module.exports = {
     // GET /
@@ -63,9 +64,25 @@ module.exports = {
             res.redirect('/');
     },
 
-    //
+    // GET /profile
     async getProfile(req, res, next) {
         const posts = await Post.find().where('author').equals(req.user._id).limit(10).exec();
         res.render('profile', { posts })
+    },
+
+    // PUT /profile
+    async updateProfile(req, res, next) {
+        const {
+            username,
+            email
+        } = req.body;
+        const { user } = res.locals;
+        if (username) user.username = username;
+        if (email) user.email = email;
+        await user.save();
+        const login = util.promisify(req.login.bind(req));
+        await login(user);
+        req.session.success = 'Profile successfully updated!';
+        res.redirect('/profile');
     }
 }
